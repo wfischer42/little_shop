5473362485d562ab48d2cf5c6eab2df71ffe45a3
Cesar Jolibois <diazcd85@gmail.com>
2018-10-23 17:17:54 -0600

    added OrderItem model with validation and tests

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,7 @@
 class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  validates :item_price, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :item_quantity, presence: true, numericality: {greater_than: 0}
 end

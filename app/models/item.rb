8be191a6028660adcfd07de7332a0efb47fa5711
Cesar Jolibois <diazcd85@gmail.com>
2018-10-23 17:06:59 -0600

    added Item model with validations and tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :order_items
+  has_many :orders, through: :order_items
+
+  validates_presence_of :name, :description, :price, :img_url, require: true
+  validates :price, numericality: {greater_than_or_equal_to: 0}
+  validates :inventory_count, numericality: {greater_than_or_equal_to: 0}
 end

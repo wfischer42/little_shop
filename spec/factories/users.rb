3a79d4d1fcda623d6710d2873b86e5df4b9de051
William Fischer <william.fischer42@gmail.com>
2018-10-23 09:52:10 -0600

    setup factorybot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    address { "MyString" }
-    city { "MyString" }
-    state { "MyString" }
-    zip_code { 1 }
-    email { "MyString" }
-    password_digest { "MyString" }
-    role { 1 }
-    active { false }
+    sequence :name        { |n| "User #{n}"}
+    sequence :address     { |n| "Address #{n}" }
+    sequence :city        { |n| "City #{n}" }
+    sequence :state       { |n| "State #{n}" }
+    sequence :zip_code    { |n| n }
+    sequence :email       { |n| "Email #{n}" }
+    sequence :password    { |n| "Password #{n}" }
+    sequence :created_at  { |n| n.day.ago }
+    role                  { 0 }
+    active                { true }
   end
 end

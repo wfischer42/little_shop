6180aaf421477e9578666f9a005b47c329911d0a
Cesar Jolibois <diazcd85@gmail.com>
2018-10-24 16:22:27 -0600

    spec file done

--- spec/features/user_go_to_profile_page_spec.rb
+++ spec/features/user_go_to_profile_page_spec.rb
@@ -4,17 +4,15 @@ describe 'User (customer,merchant,admin) and can see profile' do
 
   before do
     @user_1 = create(:user)
-    @user_2 = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_2)
 
-    visit profile_path(@user_1)
+    visit profile_path
   end
 
   context 'profile page' do
-    it { is_expected.to current_path(profile_path(@user_1)) }
-    it { is_expected.to_not current_path(profile_path(@user_1)) }
+    it { is_expected.to current_path(profile_path) }
+    it { is_expected.to have_http_status(200)}
 
     it { is_espected.to have_content(@user_1.name) }
     it { is_espected.to have_content(@user_1.address) }
@@ -22,10 +20,17 @@ describe 'User (customer,merchant,admin) and can see profile' do
     it { is_espected.to have_content(@user_1.zip_code) }
     it { is_espected.to have_content(@user_1.email) }
     it { is_espected.to_not have_content(@user_1.password) }
+
+    context 'stats' do
+      it { is_expected.to have_content(@user_1.created_at.year)}
+    end
   end
 
-  context 'Links to other parts of site' do
+  context 'Links to Order show page' do
+    subject {click_on ".order_page_link"; page}
     
+    it { is_expected.to have_current_path(user_orders_path(@user_1))}
+    it { is_expected.to have_http_status(200)}
   end
 
 end

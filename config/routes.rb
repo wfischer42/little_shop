509046e2c3f1174a5af270a288c21448876739bd
Nick Lindeberg <lindebergnick@gmail.com>
2018-10-24 14:32:50 -0600

    adds login and register links to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-
   resources :items, only: [:index]
   resources :merchants, only: [:index]
 
   root 'welcome#index'
   get '/cart', to: 'cart_items#index'
+  get '/login', to: "sessions#new"
+  get '/register', to: "users#new"
+
 
 end

0866badd791a9bdaa49aa14441a9e88a730c6956
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-10-24 19:58:58 -0600

    masterbranch data succesfully merged in

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< HEAD
 
 def seed_user(role: :customer)
   User.create(name: Faker::Name.unique.name,
@@ -66,5 +65,3 @@ end
 100.times { seed_order }
 
 add_login_paths
-=======
->>>>>>> removing original seed data before Faker data is implemented

30035a3a0dd85108c52b03cadc10ba8833db1a8f
Cesar Jolibois <diazcd85@gmail.com>
2018-10-26 11:40:51 -0600

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :merchants, only: [:index]
 
-  # scope :profile, only: [:edit] do
-  #   resources :user, only: [:edit]
-  # end
-
   root 'welcome#index'
   get '/profile', to: "users#profile"
-  get '/profile/edit/', to: "users/profile#edit"
+  get '/profile/edit/', to: "users#edit"
   get '/cart', to: 'cart_items#index'
   get '/login', to: "sessions#new"
   get '/register', to: "users#new"
--- spec/features/user_can_update_it_own_profile_spec.rb
+++ spec/features/user_can_update_it_own_profile_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
 describe 'user goes to profile page and updates it info' do
-  before do
-    @user = create(:user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+  it 'should go to the form' do
+    user = create(:user, email: "didfjslkd")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit profile_path
 
     click_on 'Update Profile'
+
+    expect(current_path).to eq(profile_edit_path)
+    expect(find_field("user_name").value).to eq(user.name)
+    expect(find_field("user_address").value).to eq(user.address)
+    expect(find_field("user_city").value).to eq(user.city)
+    expect(find_field("user_state").value).to eq(user.state)
+    expect(find_field("user_zip_code").value).to eq("#{user.zip_code}")
+    expect(find_field("user_password").value).to_not eq(user.password)
+    expect(find_field("user_email").value).to eq(user.email)
   end
-  
-  subject{page}
-
-  it { is_expected.to have_current_path(profile_edit_path) }
-  it { is_expected.to have_content(@user.name) }
-  it { is_expected.to have_content(@user.address) }
-  it { is_expected.to have_content(@user.city) }
-  it { is_expected.to have_content(@user.zip_code) }
-  it { is_expected.to have_content(@user.email) }
-  it { is_expected.to_not have_content(@user.password) }
-
-
 end

3930108c4764ede6d67d89c35d602af7c2b3a137
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-10-26 22:25:05 -0600

    completed functionality to downgrade merchant to customer

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -5,15 +5,27 @@ class Admin::MerchantsController < Admin::BaseController
   end
 
   def update
-    merchant = User.find(params[:id])
-    if merchant.active?
-      merchant.update(active: false)
-      flash[:notice] = "#{merchant.name} is now inactive."
-      redirect_to controller: "/merchants", action: "index"
-    else
-      merchant.update(active: true)
-      flash[:notice] = "#{merchant.name} is now active."
-      redirect_to controller: "/merchants", action: "index"
+    user = User.find(params[:id])
+    if params[:attribute] == 'active'
+      if user.active?
+        user.update(active: false)
+        flash[:notice] = "#{user.name} is now inactive."
+        redirect_to controller: "/merchants", action: "index"
+      else
+        user.update(active: true)
+        flash[:notice] = "#{user.name} is now active."
+        redirect_to controller: "/merchants", action: "index"
+      end
+    elsif params[:attribute] == 'role'
+      if user.customer?
+        user.update(role: 1)
+        flash[:notice] = "#{user.name} is now a merchant."
+        redirect_to controller: "/merchants", action: "index"
+      elsif user.merchant?
+        user.update(role: 0)
+        flash[:notice] = "#{user.name} is now a customer only."
+        redirect_to controller: "/merchants", action: "index"
+      end
     end
   end
 
--- spec/features/admin_can_edit_merchant_profile_data_spec.rb
+++ spec/features/admin_can_edit_merchant_profile_data_spec.rb
@@ -1,3 +1,16 @@
 require 'rails_helper'
 
-describe 
+describe 'Admin goes to merchant profile page' do
+  it 'can downgrade a merchant to a user' do
+    admin =  create(:user, role: 2)
+    merchant = create(:user, role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit admin_merchant_path(merchant)
+
+    click_button("Downgrade to Customer Only Account")
+    merchant_rename = User.find(merchant.id)
+    expect(merchant_rename.role).to eq('customer')
+  end
+
+end

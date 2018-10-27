4f5758c6fd5de2ceea51b9bc014510858c631411
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-10-27 11:39:41 -0600

    created partial for editing profile data, need to complete testing

--- spec/features/admin_can_edit_merchant_profile_data_spec.rb
+++ spec/features/admin_can_edit_merchant_profile_data_spec.rb
@@ -13,4 +13,15 @@ describe 'Admin goes to merchant profile page' do
     expect(merchant_rename.role).to eq('customer')
   end
 
+  it 'can edit profile information' do
+    admin =  create(:user, role: 2)
+    merchant = create(:user, role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit admin_merchant_path(merchant)
+
+    click_link("Edit Profile Information")
+    expect(current_path).to eq(edit_admin_merchant_path(merchant))
+    expect(page).to have_content("Update User")
+  end
 end

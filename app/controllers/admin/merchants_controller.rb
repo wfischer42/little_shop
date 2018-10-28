09ad6ad7421bf0a572a802d5240c6c8199f52c62
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-10-28 17:04:16 -0600

    refactors routes in admin merhants and admin users controller under update

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -13,28 +13,27 @@ class Admin::MerchantsController < Admin::BaseController
 
   def update
     user = User.find(params[:id])
-    update_redirect = admin_merchant_path(user)
     if params[:attribute] == 'active'
       if user.active?
         user.update(active: false)
         flash[:notice] = "#{user.name} is now inactive."
-        redirect_to controller: "/merchants", action: "index"
+        redirect_to merchants_path
       else
         user.update(active: true)
         flash[:notice] = "#{user.name} is now active."
-        redirect_to controller: "/merchants", action: "index"
+        redirect_to merchants_path
       end
     elsif params[:attribute] == 'role'
         user.update(role: 0)
         flash[:notice] = "#{user.name} is now a customer only."
-        redirect_to controller: "admin/users", action: "show"
+        redirect_to admin_user_path(user)
     else
       if user.update(user_params)
         flash[:success] = "User information Updated"
-      redirect_to update_redirect
+      redirect_to admin_merchant_path(user)
       else
         flash[:notice] = "Error"
-        redirect_to update_redirect
+        redirect_to admin_merchant_path(user)
       end
     end
   end

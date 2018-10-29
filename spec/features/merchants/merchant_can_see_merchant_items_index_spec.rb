23218cd1fe8d9a13fd4ecae5307b807b4af5e9a4
Nick Lindeberg <lindebergnick@gmail.com>
2018-10-29 17:29:48 -0600

    adds testing for enable/disable buttons

--- spec/features/merchants/merchant_can_see_merchant_items_index_spec.rb
+++ spec/features/merchants/merchant_can_see_merchant_items_index_spec.rb
@@ -1,22 +1,39 @@
 require 'rails_helper'
 
 describe 'Merchant items index' do
+  before(:each) do
+    @merchant = create(:user, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
+    @item, @item_2 = create_list(:item, 2, user: @merchant)
+    @item_3 = create(:item)
+    visit merchant_items_path
+  end
+
   it 'lets merchant view their items only' do
-    merchant = create(:user, role: 1)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
-    item, item_2 = create_list(:item, 2, user: merchant)
-    item_3 = create(:item)
+    expect(page).to have_content(@item.name)
+    expect(page).to have_content(@item.id)
+    expect(page).to have_content(@item.price)
+    expect(page).to have_content(@item.inventory_count)
+    expect(page).to have_content(@item_2.name)
+    expect(page).to have_content(@item_2.id)
+    expect(page).to have_content(@item_2.price)
+    expect(page).to have_content(@item_2.inventory_count)
+    expect(page).to_not have_content(@item_3.name)
+  end
+
+  it 'lets merchant disble/enable an item' do
+    first(:button, "Disable").click
+
+    expect(current_path).to eq(merchant_items_path)
+    visit items_path
+
+    expect(page).to_not have_content(@item.name)
 
     visit merchant_items_path
+    first(:button, "Enable").click
 
-    expect(page).to have_content(item.name)
-    expect(page).to have_content(item.id)
-    expect(page).to have_content(item.price)
-    expect(page).to have_content(item.inventory_count)
-    expect(page).to have_content(item_2.name)
-    expect(page).to have_content(item_2.id)
-    expect(page).to have_content(item_2.price)
-    expect(page).to have_content(item_2.inventory_count)
-    expect(page).to_not have_content(item_3.name)
+    expect(page).to have_content(@item.name)
   end
+
+
 end

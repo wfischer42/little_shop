5f009d63463d7bbbf823f0df632a2db1126aab46
Nick Lindeberg <lindebergnick@gmail.com>
2018-10-30 11:55:38 -0600

    adds new item for merchant with tests for edge cases

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -4,7 +4,22 @@ class Merchant::ItemsController < Merchant::BaseController
     @user = User.find(current_user.id)
     @controller = 'merchant/items'
     @items = Item.where(user_id: @user.id)
+  end
 
+  def new
+    @item = Item.new()
+    @url = merchant_items_path
+  end
+
+  def create
+    @item = current_user.items.create(item_params)
+    if @item.save
+      flash[:success] = "#{@item.name} added to your items"
+      redirect_to merchant_items_path
+    else
+      flash[:notice] = "Item not added"
+      render :new
+    end
   end
 
   def update
@@ -20,5 +35,12 @@ class Merchant::ItemsController < Merchant::BaseController
         redirect_to merchant_items_path
       end
     end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :img_url, :price, :inventory_count)
   end
 end

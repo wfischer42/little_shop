48675ba48b1d823cba4cf657b267ba54e8218836
William Fischer <william.fischer42@gmail.com>
2018-10-31 09:14:36 -0600

    Finished the fix

--- spec/features/users/user_can_update_it_own_profile_spec.rb
+++ spec/features/users/user_can_update_it_own_profile_spec.rb
@@ -1,30 +1,11 @@
 require 'rails_helper'
 
 describe 'user goes to profile page and updates info' do
-  # include RSpec::Rails::ControllerExampleGroup
-  # before do
-  #   user = create(:user, email: "didfjslkd", password: "abcd")
-  #   # post login_path, :email => user.email, :password => 'abcd'
-  #   page.driver.post(login_path, email: user.email, password: 'abcd')
-  #   # allow(@family).to receive(:location).and_return('first', 'second', 'other')
-  #   # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(User.find(user.id))
-  #   visit profile_path
-  #   click_on 'Update Profile'
-  #   fill_in :user_name, with: "Joe"
-  #   fill_in :user_address, with: "123 Main Street"
-  #   fill_in :user_city, with: "Fakeville"
-  #   fill_in :user_state, with: "Alabama"
-  #   fill_in :user_zip_code, with: 35622
-  #   fill_in :user_email, with: 'joe@joecodes.com'
-  #   fill_in :user_password, with: 'JoeIsTheCoolest'
-  #   fill_in :user_password_confirmation, with: 'JoeIsTheCoolest'
-  # end
   let(:user) { User.first }
   it 'should redirect to profile after submitting' do
-
     user = create(:user, email: "didfjslkd", password: "abcd")
     page.driver.post(login_path, email: user.email, password: 'abcd')
-    
+
     visit profile_path
     click_on 'Update Profile'
     fill_in :user_name, with: "Joe"
@@ -48,9 +29,9 @@ describe 'user goes to profile page and updates info' do
 
   it 'should display an error if the user type thr wrong info' do
 
-    user = create(:user, email: "didfjslkd")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
+    user = create(:user, email: "didfjslkd", password: "abcd")
+    page.driver.post(login_path, email: user.email, password: 'abcd')
+    
     visit profile_path
 
     click_on 'Update Profile'

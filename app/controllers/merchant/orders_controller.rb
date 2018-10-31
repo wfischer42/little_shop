3c351947bf6ab5beb1f985d15e572ac6d54c152e
Cesar Jolibois <diazcd85@gmail.com>
2018-10-30 19:39:46 -0600

    WIP

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -3,9 +3,16 @@ class Merchant::OrdersController < Merchant::BaseController
   def index
     @merc = User.find(current_user.id)
     @orders = @merc.merchant_orders.uniq
+    # add path from here
   end
 
   def show
     @order = Order.find(params[:id])
+  end
+
+  def cancel
+    Order.find(params[:order_id]).update(status: :canceled)
+    @orders = Order.all
+    render :index
   end
 end

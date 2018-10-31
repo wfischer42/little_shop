3af7f249ec4aaaaa2515abb88b5616eb60bb0360
Cesar Jolibois <diazcd85@gmail.com>
2018-10-31 12:22:38 -0600

    Small changes

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -9,10 +9,4 @@ class Merchant::OrdersController < Merchant::BaseController
     @order = Order.find(params[:id])
   end
 
-  def cancel
-    @order = Order.find(params[:order_id])
-    @order.update(status: :canceled)
-    @orders = Order.all
-    render :show
-  end
 end
